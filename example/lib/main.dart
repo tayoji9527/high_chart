fc5d941c506747edc3addce478d8877a8735fa77
tayoji9527 <tayoji9527@163.com>
2021-06-30 14:35:45 +0800

    null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:high_chart/high_chart.dart';
 
 void main() {
+  FlutterError.onError = (err) async {};
   runApp(MyApp());
 }
 
@@ -62,39 +63,59 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-}
 
-const aaa = '''{
-    "extension": {
-        "layout": "single",
-        "col": 4,
-        "height": "300px"
-    },
-    "title": {
-        "text": "饼图2"
-    },
-    "series": [
-        {
-            "type": "pie",
-            "name": "one",
-            "data": [
-                [
-                    "A",
-                    21.626222464683842
-                ],
-                [
-                    "B",
-                    25
-                ],
-                [
-                    "C",
-                    25
-                ],
-                [
-                    "D",
-                    25
-                ]
-            ]
-        }
-    ]
+  String get aaa => ''' {
+	chart : {
+		events : {
+			load : function () {
+				// set up the updating of the chart each second
+				var series = this.series[0];
+				setInterval(function () {
+					var x = (new Date()).getTime(), // current time
+						y = Math.round(Math.random() * 100);
+					series.addPoint([x, y], true, true);
+				}, 1000);
+			}
+		}
+	},
+	rangeSelector: {
+		buttons: [{
+			count: 1,
+			type: 'minute',
+			text: '1M'
+		}, {
+			count: 5,
+			type: 'minute',
+			text: '5M'
+		}, {
+			type: 'all',
+			text: 'All'
+		}],
+		inputEnabled: false,
+		selected: 0
+	},
+	title : {
+		text : 'Live random data'
+	},
+	tooltip: {
+		split: false
+	},
+	exporting: {
+		enabled: false
+	},
+	series : [{
+		name : '随机数据',
+		data : (function () {
+			// generate an array of random data
+			var data = [], time = (new Date()).getTime(), i;
+			for (i = -50; i <= 0; i += 1) {
+				data.push([
+					time + i * 1000,
+					Math.round(Math.random() * 100)
+				]);
+			}
+			return data;
+		}())
+	}]
 }''';
+}
--- lib/high_chart.dart
+++ lib/high_chart.dart
@@ -8,11 +8,11 @@ import 'package:high_chart/high_chart_script.dart';
 import 'package:high_chart/high_stock_script.dart';
 
 class HighCharts extends StatefulWidget {
-  final Function(bool isLoadStop) onLoad;
-  HighCharts({Key key, this.data, this.onLoad, this.isStock = true})
+  final Function(bool isLoadStop)? onLoad;
+  HighCharts({Key? key, this.data, this.onLoad, this.isStock = true})
       : super(key: key);
 
-  final String data;
+  final String? data;
   final bool isStock;
 
   @override
@@ -24,10 +24,10 @@ class HighCharts extends StatefulWidget {
 //     'PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PG1ldGEgY2hhcnNldD0idXRmLTgiPjxtZXRhIG5hbWU9InZpZXdwb3J0IiBjb250ZW50PSJ3aWR0aD1kZXZpY2Utd2lkdGgsIGluaXRpYWwtc2NhbGU9MS4wLCBtYXhpbXVtLXNjYWxlPTEuMCwgbWluaW11bS1zY2FsZT0xLjAsIHVzZXItc2NhbGFibGU9MCIgLz48L2hlYWQ+PGJvZHk+PGRpdiBpZD0iY2hhcnQiPjxoMT5QbGVhc2UgV2FpdCAhISEgPC9oMT48L2Rpdj48L2JvZHk+PC9odG1sPjxzY3JpcHQ+ZnVuY3Rpb24gc2VudGhpbG5hc2EoYSl7IGV2YWwoYSk7IHJldHVybiB0cnVlO30NCjwvc2NyaXB0PjwvaHRtbD4=';
 
 class _HighChartsState extends State<HighCharts> {
-  String get _currentData => widget.data;
+  String get _currentData => widget.data ?? '';
   final GlobalKey webViewKey = GlobalKey();
 
-  InAppWebViewController _webViewController;
+  InAppWebViewController? _webViewController;
 
   @override
   void initState() {
@@ -41,40 +41,49 @@ class _HighChartsState extends State<HighCharts> {
 
   void update() async {
     if (widget.isStock)
-      await _webViewController?.callAsyncJavaScript(functionBody: '''
+      await _webViewController?.evaluateJavascript(source: '''
         var a= senthilnasa(`Highcharts.stockChart('chart',
-        ${_currentData ?? ''}
+        $_currentData
         )`);
     ''');
     else
-      await _webViewController?.callAsyncJavaScript(functionBody: '''
+      await _webViewController?.evaluateJavascript(source: '''
         var a= senthilnasa(`Highcharts.chart('chart',
-        ${_currentData ?? ''}
+        $_currentData
         )`);
+
     ''');
   }
 
   String htmlData() =>
-      '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0" /> <style type="text/css">html, body,div {touch-action: none;-ms-touch-action: none;height:100%;-webkit-touch-callout:none;-webkit-user-select:none; -khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;}</style></head><body><div id="chart"></div></body></html><script>${widget.isStock ? highstockScript : highchartsScript} function senthilnasa(a){ eval(a); return true;}</script></html>';
+      '''<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0" /> <style type="text/css">html, body,div {touch-action: none;-ms-touch-action: none;height:100%;-webkit-touch-callout:none;-webkit-user-select:none; -khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;}</style></head><body><div id="chart"></div></body></html><script>
+      ${widget.isStock ? highstockScript : highchartsScript} 
+       function senthilnasa(a){ eval(a); return true;}
+      </script></html>''';
 
   @override
   Widget build(BuildContext context) {
     update();
-
     return InAppWebView(
       initialData: InAppWebViewInitialData(data: htmlData()),
       onWebViewCreated: (controller) async {
         _webViewController = controller;
-        update();
-        if (widget.onLoad != null) widget.onLoad(false);
+        if (widget.onLoad != null) widget.onLoad!(false);
       },
       onLoadStop: (controller, url) {
         update();
-        if (widget.onLoad != null) widget.onLoad(true);
+        if (widget.onLoad != null) widget.onLoad!(true);
       },
       onLoadStart: (controller, url) {},
       initialOptions: InAppWebViewGroupOptions(
-          crossPlatform: InAppWebViewOptions(transparentBackground: true)),
+          crossPlatform: InAppWebViewOptions(
+        transparentBackground: true,
+        allowUniversalAccessFromFileURLs: true,
+        javaScriptCanOpenWindowsAutomatically: true,
+        javaScriptEnabled: true,
+        horizontalScrollBarEnabled: false,
+        verticalScrollBarEnabled: false,
+      )),
     );
   }
 }
